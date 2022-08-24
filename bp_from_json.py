10727f39ad8f988007a006629de0d833793a1d15
flameSla <39356103+flameSla@users.noreply.github.com>
2022-08-24 20:45:06 +0700

    update bp_from_json.py

--- bp_from_json.py
+++ bp_from_json.py
@@ -373,172 +373,15 @@ class blueprint:
 
         return cls.from_string(exchange_str)
 
-    def to_str(self):
-        json_str = json.dumps(self.data,
-                              separators=(",", ":"),
-                              ensure_ascii=False).encode("utf8")
-        exchange_str = '0' + base64.b64encode(zlib.compress(json_str,
-                                                            9)).decode('utf-8')
-        return exchange_str
-
-    def __get_all_items_parse(self, bp, items):
-        if bp.is_blueprint_book():
-            for b in bp.read_blueprints():
-                self.__get_all_items_parse(blueprint(b), items)
-        elif bp.is_blueprint():
-            for entity in bp.get_entities():
-                if entity.read_name() == 'curved-rail':
-                    items += {"rail": 4}
-                elif entity.read_name() == 'straight-rail':
-                    items += {"rail": 1}
-                else:
-                    items += {entity.read_name(): 1}
-                items += entity.read_items()
-
-    def get_all_items(self):
-        items = dict_bp()
-        self.__get_all_items_parse(self, items)
-        return items
-
-    def __get_all_tiles_parse(self, bp, tiles):
-        if bp.is_blueprint_book():
-            for b in bp.read_blueprints():
-                self.__get_all_tiles_parse(blueprint(b), tiles)
-        elif bp.is_blueprint():
-            for t in bp.read_tiles():
-                tiles += {t['name']: 1}
-
-    def get_all_tiles(self):
-        tiles = dict_bp()
-        self.__get_all_tiles_parse(self, tiles)
-        return tiles
-
-    def __get_all_bp_parse(self, bp, bps, current_directory):
-        if bp.is_blueprint_book():
-            md5 = bp.get_md5()
-            current_directory += md5 + '\\'
-            bps.append([bp, md5, current_directory])
-            for b in bp.read_blueprints():
-                self.__get_all_bp_parse(blueprint(b), bps, current_directory)
-        elif bp.is_blueprint():
-            md5 = bp.get_md5()
-            bps.append([bp, md5, current_directory])
-
-    def get_all_bp(self,
-                   onedimensional=False,
-                   blueprint_only=False):
-        bps = list()
-        self.__get_all_bp_parse(self, bps, '')
-
-        if onedimensional is False:
-            return bps
-        elif onedimensional is True:
-            temp = list()
-            for a in bps:
-                if blueprint_only is False:
-                    temp.append(a[0])
-                else:
-                    if a[0].is_blueprint():
-                        temp.append(a[0])
-            return temp
-        else:
-            return list()
-
-    def is_blueprint_book(self):
-        if self.data is not None:
-            return 'blueprint_book' in self.data
-
-    def is_blueprint(self):
-        if self.data is not None:
-            return 'blueprint' in self.data
-
-    def read_blueprints(self):
-        return self.obj.get('blueprints', list())
+# -------------------------------------
+#   append
 
     def append_entity(self, e):
         e.set_entity_number(len(self.obj['entities']) + 1)
         self.obj['entities'].append(e.data)
 
-    def get_entities(self):
-        return list(map(lambda x: entity(x), self.obj['entities']))
-
-    def print_entities(self):
-        for e in self.get_entities():
-            print(e.data)
-            # print(e.get_pos(), " ", e.data)
-
-    def normalize_entities(self):
-        # position -= pos_min
-        x = list()
-        y = list()
-        entities = self.get_entities()
-        for e in entities:
-            x.append(e.get_pos().read_x())
-            y.append(e.get_pos().read_y())
-
-        pmin = position.new_position(min(x), min(y))
-        for e in entities:
-            e.get_pos().__isub__(pmin)
-        return pmin
-
-    def denormalization_entities(self, p):
-        for e in self.get_entities():
-            e.get_pos().__iadd__(p)
-
-    def read_tiles(self):
-        return self.obj.get('tiles', list())
-
-    def read_label(self):
-        return self.obj.get('label', 'untitled')
-
-    def set_label(self, str):
-        self.obj['label'] = str
-
-    def read_description(self):
-        return self.obj.get('description', 'description is missing')
-
-    def set_description(self, str):
-        self.obj['description'] = str
-
-    def read_label_color(self):
-        return self.obj.get('label_color', None)
-
-    def set_label_color(self, r, g, b):
-        self.obj['label_color'] = {"r": r, "g": g, "b": b}
-
-    def read_icons(self):
-        return self.obj.get('icons', None)
-
-    def read_schedules(self):
-        return self.obj.get('schedules', list())
-
-    def read_version(self):
-        return self.obj.get('version', '')
-
-    def set_icons(self, index, icon_type, name):
-        new_icon = {'signal': {'type': icon_type, 'name': name},
-                    'index': index}
-        if index >= 1 and index <= 4:
-            if 'icons' in self.obj:
-                if index in [icon['index'] for icon in self.obj['icons']]:
-                    for i in range(len(self.obj['icons'])):
-                        if self.obj['icons'][i]['index'] == index:
-                            self.obj['icons'][i] = new_icon
-                            break
-                else:
-                    self.obj['icons'].append(new_icon)
-            else:
-                self.obj['icons'] = [new_icon]
-
-    def read_item(self):
-        return self.obj['item']
-
-    def __md5(self, data):
-        return hashlib.md5(json.dumps(data,
-                           sort_keys=True).encode('utf-8')).hexdigest()
-
-    def get_md5(self):
-        return self.__md5(self.data)
+# -------------------------------------
+#   compare
 
     def __compare_entities(self, bp, debug):
         e1 = sorted(self.get_entities(),
@@ -616,3 +459,189 @@ class blueprint:
                 return self.__compare_bp_book(bp, debug)
             else:
                 return bp.__compare_bp_book(self, debug)
+
+# -------------------------------------
+#   get_all
+
+    def __get_all_items_parse(self, bp, items):
+        if bp.is_blueprint_book():
+            for b in bp.read_blueprints():
+                self.__get_all_items_parse(blueprint(b), items)
+        elif bp.is_blueprint():
+            for entity in bp.get_entities():
+                if entity.read_name() == 'curved-rail':
+                    items += {"rail": 4}
+                elif entity.read_name() == 'straight-rail':
+                    items += {"rail": 1}
+                else:
+                    items += {entity.read_name(): 1}
+                items += entity.read_items()
+
+    def get_all_items(self):
+        items = dict_bp()
+        self.__get_all_items_parse(self, items)
+        return items
+
+    def __get_all_tiles_parse(self, bp, tiles):
+        if bp.is_blueprint_book():
+            for b in bp.read_blueprints():
+                self.__get_all_tiles_parse(blueprint(b), tiles)
+        elif bp.is_blueprint():
+            for t in bp.read_tiles():
+                tiles += {t['name']: 1}
+
+    def get_all_tiles(self):
+        tiles = dict_bp()
+        self.__get_all_tiles_parse(self, tiles)
+        return tiles
+
+    def __get_all_bp_parse(self, bp, bps, current_directory):
+        if bp.is_blueprint_book():
+            md5 = bp.get_md5()
+            current_directory += md5 + '\\'
+            bps.append([bp, md5, current_directory])
+            for b in bp.read_blueprints():
+                self.__get_all_bp_parse(blueprint(b), bps, current_directory)
+        elif bp.is_blueprint():
+            md5 = bp.get_md5()
+            bps.append([bp, md5, current_directory])
+
+    def get_all_bp(self,
+                   onedimensional=False,
+                   blueprint_only=False):
+        bps = list()
+        self.__get_all_bp_parse(self, bps, '')
+
+        if onedimensional is False:
+            return bps
+        elif onedimensional is True:
+            temp = list()
+            for a in bps:
+                if blueprint_only is False:
+                    temp.append(a[0])
+                else:
+                    if a[0].is_blueprint():
+                        temp.append(a[0])
+            return temp
+        else:
+            return list()
+
+# -------------------------------------
+#   get_
+
+    def get_entities(self):
+        return list(map(lambda x: entity(x), self.obj['entities']))
+
+    def __md5(self, data):
+        return hashlib.md5(json.dumps(data,
+                           sort_keys=True).encode('utf-8')).hexdigest()
+
+    def get_md5(self):
+        return self.__md5(self.data)
+
+# -------------------------------------
+#   is_
+
+    def is_blueprint_book(self):
+        if self.data is not None:
+            return 'blueprint_book' in self.data
+
+    def is_blueprint(self):
+        if self.data is not None:
+            return 'blueprint' in self.data
+
+# -------------------------------------
+#   normalize_
+
+    def denormalization_entities(self, p):
+        for e in self.get_entities():
+            e.get_pos().__iadd__(p)
+
+    def normalize_entities(self):
+        # position -= pos_min
+        x = list()
+        y = list()
+        entities = self.get_entities()
+        for e in entities:
+            x.append(e.get_pos().read_x())
+            y.append(e.get_pos().read_y())
+
+        pmin = position.new_position(min(x), min(y))
+        for e in entities:
+            e.get_pos().__isub__(pmin)
+        return pmin
+
+# -------------------------------------
+#   print_
+
+    def print_entities(self):
+        for e in self.get_entities():
+            print(e.data)
+
+# -------------------------------------
+#   read_
+
+    def read_blueprints(self):
+        return self.obj.get('blueprints', list())
+
+    def read_description(self):
+        return self.obj.get('description', 'description is missing')
+
+    def read_icons(self):
+        return self.obj.get('icons', None)
+
+    def read_item(self):
+        return self.obj['item']
+
+    def read_label(self):
+        return self.obj.get('label', 'untitled')
+
+    def read_label_color(self):
+        return self.obj.get('label_color', None)
+
+    def read_schedules(self):
+        return self.obj.get('schedules', list())
+
+    def read_tiles(self):
+        return self.obj.get('tiles', list())
+
+    def read_version(self):
+        return self.obj.get('version', '')
+
+# -------------------------------------
+#   set_
+
+    def set_description(self, str):
+        self.obj['description'] = str
+
+    def set_icons(self, index, icon_type, name):
+        new_icon = {'signal': {'type': icon_type, 'name': name},
+                    'index': index}
+        if index >= 1 and index <= 4:
+            if 'icons' in self.obj:
+                if index in [icon['index'] for icon in self.obj['icons']]:
+                    for i in range(len(self.obj['icons'])):
+                        if self.obj['icons'][i]['index'] == index:
+                            self.obj['icons'][i] = new_icon
+                            break
+                else:
+                    self.obj['icons'].append(new_icon)
+            else:
+                self.obj['icons'] = [new_icon]
+
+    def set_label(self, str):
+        self.obj['label'] = str
+
+    def set_label_color(self, r, g, b):
+        self.obj['label_color'] = {"r": r, "g": g, "b": b}
+
+# -------------------------------------
+#   to_
+
+    def to_str(self):
+        json_str = json.dumps(self.data,
+                              separators=(",", ":"),
+                              ensure_ascii=False).encode("utf8")
+        exchange_str = '0' + base64.b64encode(zlib.compress(json_str,
+                                                            9)).decode('utf-8')
+        return exchange_str
