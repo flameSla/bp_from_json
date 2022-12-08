c6e86279a6ed1d6b12a77d8cc18f95295133035a
flameSla <39356103+flameSla@users.noreply.github.com>
2022-12-08 12:24:48 +0700

    Update example_calculating_the_mall_for_a_cell.py

--- example_calculating_the_mall_for_a_cell.py
+++ example_calculating_the_mall_for_a_cell.py
@@ -2,17 +2,17 @@
 # calculating the mall for a cell
 #
 
-import json
-import zlib
+import argparse
 import base64
-#from urllib.parse import quote, unquote
+import json
+import os
+import sys
+import zlib
 from bp_from_json import blueprint
 from bp_from_json import dict_bp
-#from bp_from_json import entity
-#from bp_from_json import get_items
 from fractions import Fraction
-import argparse
-import os
+# from urllib.parse import quote, unquote
+
 
 productivity = {'iron-gear-wheel': Fraction(1.4),
                 'iron-stick': Fraction(1.4),
@@ -23,16 +23,16 @@ productivity = {'iron-gear-wheel': Fraction(1.4),
                 'sulfur': Fraction(1.3),
                 'lubricant': Fraction(1.3),
                 'copper-cable': Fraction(1.4),
-                'plastic-bar': Fraction(1.3), 
+                'plastic-bar': Fraction(1.3),
                 'steel-plate': Fraction(1.2),
                 'iron-plate': Fraction(1.2),
                 'copper-plate': Fraction(1.2),
                 'stone-brick': Fraction(1.2),
-                
+
                 'stack-inserter': Fraction(1.0),
                 'fast-inserter': Fraction(1.0),
                 'inserter': Fraction(1.0),
-                
+
                 'fast-transport-belt': Fraction(1.0),
                 'transport-belt': Fraction(1.0),
                 'fast-underground-belt': Fraction(1.0),
@@ -41,16 +41,16 @@ productivity = {'iron-gear-wheel': Fraction(1.4),
                 'splitter': Fraction(1.0),
 
                 'steel-chest': Fraction(1.0),
-                
+
                 'assembling-machine-2': Fraction(1.0),
                 'assembling-machine-1': Fraction(1.0),
-                
+
                 'speed-module': Fraction(1.0),
                 'pipe': Fraction(1.0),
 
                 'engine-unit': Fraction(1.4),
                 'electric-engine-unit': Fraction(1.4),
-                
+
                 'concrete': Fraction(1.0)}
 
 
@@ -72,12 +72,13 @@ nuclear_power_plant_1120MW = {'accumulator': 4,
                               'stack-inserter': 16,
                               'steam-turbine': 224,
                               'substation': 22}
-                                            
+
 
 #############################################
 def debug(*args):
     if opt.d:
         print(*args, file=sys.stderr, flush=True)
+
 
 #############################################
 # https://stackoverflow.com/questions/46351275/using-pako-deflate-with-python
@@ -93,10 +94,18 @@ def pako_deflate_raw(data):
     compress = zlib.compressobj(
         zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -15, memLevel=8,
         strategy=zlib.Z_DEFAULT_STRATEGY)
-    #compressed_data = compress.compress(js_string_to_byte(js_encode_uri_component(data)))
+    # compressed_data = compress.compress(js_string_to_byte(js_encode_uri_component(data)))
     compressed_data = compress.compress(js_string_to_byte(data))
     compressed_data += compress.flush()
     return compressed_data
+
+
+def js_encode_uri_component(data):
+    return quote(data, safe='~()*!.\'')
+
+
+def js_decode_uri_component(data):
+    return unquote(data)
 
 
 def js_string_to_byte(data):
@@ -133,14 +142,11 @@ def get_recipes():
     # json -> dist()
     recipes = dict()
     for r in json_all['recipes']:
-        if len(r['products'])==1:
+        if len(r['products']) == 1:
             for ingredient in r['ingredients']:
-                #ingredient['amount'] /= r['products'][0]['amount']
-                #print(r)
-                #print(r['name'], ingredient['amount'], r['products'][0]['amount'])
-                ingredient['amount'] = Fraction(ingredient['amount'], r['products'][0]['amount'])
+                ingredient['amount'] = Fraction(ingredient['amount'],
+                                                r['products'][0]['amount'])
             recipes[r['name']] = r['ingredients']
-            #print(r)
 
     return recipes
 
@@ -149,7 +155,7 @@ def get_recipes():
 def get_all_ingredients(ingredient, amount, final_ingredients):
     res = dict_bp()
     debug('recurcive')
-    debug('\t',ingredient, amount)
+    debug('\t', ingredient, amount)
     if isinstance(ingredient, dict):
         if not ingredient['name'] in final_ingredients\
            and ingredient['name'] in recipes:
@@ -160,10 +166,12 @@ def get_all_ingredients(ingredient, amount, final_ingredients):
                 k = Fraction(1)
                 print("'{}': Fraction(1.0),".format(ingredient['name']))
             for i in recipes[ingredient['name']]:
-                res += get_all_ingredients(i, ingredient['amount'] * amount / k, final_ingredients)
+                res += get_all_ingredients(i,
+                                           ingredient['amount'] * amount / k,
+                                           final_ingredients)
         else:
             res = dict_bp({ingredient['name']: amount * ingredient['amount']})
-    
+
     return res
 
 
@@ -171,9 +179,10 @@ def get_all_ingredients(ingredient, amount, final_ingredients):
 def print_dict(d, dimension=None):
     if dimension is None:
         dimension = ''
-    for k, v in sorted(d.items(), key=lambda x: x[0]): 
+    for k, v in sorted(d.items(), key=lambda x: x[0]):
         print('{:34} = {:10.3f} {}'.format(k, float(v), dimension))
-        
+
+
 ######################################
 #
 # main
@@ -181,8 +190,8 @@ if __name__ == "__main__":
 
     # kirkmcdonald.github.io
     # zip -> settings
-    #zip=r'lVTbktowDP2bPMUDBNjZ0snHGEcJHnyrbLNsv77Kjdy823ZgmEE6R5KPLhUPvDww+vzItDTlMbuCCiU8HYL3LCA33lkMrDVnlS7dMXtIX17tM5MBtC+FNT5gFEFaw9Bebbjg5e2cK9tIH6SY2zgGqRTgJ/M3UKo17nPFPSALERFa2Om8O+VNNJOlKM6797xWXEO4of3YoF1n46gtMn0vyHzYFfmVh9CmIguDX1E6DaalHPI6+q5Y4CIQZeEkogP01nDF+soqqMF4WMV4gdrndQpt8lAo72QFGNCajgRP6++gIFD2NZQHq0mtq9XX9s2H3WGfR5JfRs3qCIqJXrHiNNnRRlN5pnnDf0sDbaTjOTdRKODYscj0vj/tinMmG2MRSh+AYjnFA+RoxR1Ch8u0raICX/LqwY2AigmJIspw+emPl8PbpnUmbn4WT0j5h450IYvTq0GTEt+QVvKmkIlJTMMcNa+XoC9ln0NdA/EeMlCiTokkdQtjxb8Cj2mgIhySaGAa6iCLZpCc9Ol91pB31Yz0HCXjP52yXj6ACW4Mwb/uXi1pIVBP05QEJbaQahq/5P+UpunVZzUSeHzNl0uXSjPb/3l0SWqwpp3tjxuN8Ri6My/6ebcPjvAkVUnbW5uGOZp2OmkjZ3F15jlWdytVnbIfdBWMb7vbT1zE1zsX2zfY/npRUlm2F+Z/8X9fGScBRd+w5SVJgx0MOs2Mq9ubJPbSt3le801TjLzimCQMh4kWmuZfLZZicCkejbhBmu4d0AH7Zo/ngC82eHa5k+7pjI7P8VHVEcc6+380RlzIajRu7/nAHR2TUmvKukGzkZ1vAZ34aZRH7tz4Bw=='
-    #print(js_bytes_to_string(pako_inflate_raw(js_atob(zip))).split('&')[5])
+    # zip=r'lVTbktowDP2bPMUDBNjZ0snHGEcJHnyrbLNsv77Kjdy823ZgmEE6R5KPLhUPvDww+vzItDTlMbuCCiU8HYL3LCA33lkMrDVnlS7dMXtIX17tM5MBtC+FNT5gFEFaw9Bebbjg5e2cK9tIH6SY2zgGqRTgJ/M3UKo17nPFPSALERFa2Om8O+VNNJOlKM6797xWXEO4of3YoF1n46gtMn0vyHzYFfmVh9CmIguDX1E6DaalHPI6+q5Y4CIQZeEkogP01nDF+soqqMF4WMV4gdrndQpt8lAo72QFGNCajgRP6++gIFD2NZQHq0mtq9XX9s2H3WGfR5JfRs3qCIqJXrHiNNnRRlN5pnnDf0sDbaTjOTdRKODYscj0vj/tinMmG2MRSh+AYjnFA+RoxR1Ch8u0raICX/LqwY2AigmJIspw+emPl8PbpnUmbn4WT0j5h450IYvTq0GTEt+QVvKmkIlJTMMcNa+XoC9ln0NdA/EeMlCiTokkdQtjxb8Cj2mgIhySaGAa6iCLZpCc9Ol91pB31Yz0HCXjP52yXj6ACW4Mwb/uXi1pIVBP05QEJbaQahq/5P+UpunVZzUSeHzNl0uXSjPb/3l0SWqwpp3tjxuN8Ri6My/6ebcPjvAkVUnbW5uGOZp2OmkjZ3F15jlWdytVnbIfdBWMb7vbT1zE1zsX2zfY/npRUlm2F+Z/8X9fGScBRd+w5SVJgx0MOs2Mq9ubJPbSt3le801TjLzimCQMh4kWmuZfLZZicCkejbhBmu4d0AH7Zo/ngC82eHa5k+7pjI7P8VHVEcc6+380RlzIajRu7/nAHR2TUmvKukGzkZ1vAZ34aZRH7tz4Bw=='
+    # print(js_bytes_to_string(pako_inflate_raw(js_atob(zip))).split('&')[5])
 
     exchange_str = ''
     parser = argparse.ArgumentParser(
@@ -191,36 +200,39 @@ if __name__ == "__main__":
                         help="debug output on STDERR")
     opt = parser.parse_args()
 
-    exchange_str = input_def('cell BP (string or filename.txt)','cell500spm.txt')
+    exchange_str = input_def('cell BP (string or filename.txt)',
+                             'cell500spm.txt')
     if os.path.exists(exchange_str):
         bp = blueprint.from_file(exchange_str)
     else:
         bp = blueprint.from_string(exchange_str)
 
     necessary_items_for_construction = bp.get_all_items()
-    debug(json.dumps(necessary_items_for_construction, indent=4, sort_keys=True))
+    debug(json.dumps(necessary_items_for_construction,
+          indent=4, sort_keys=True))
     if 'speed-module-3' in necessary_items_for_construction:
         necessary_items_for_construction1 = dict_bp()
+
         print()
         speed_modules_per_minute = Fraction(input_def('How many speed modules are produced per minute?', '49.4'))
-
         construction_time = necessary_items_for_construction['speed-module-3']/speed_modules_per_minute
-
         print('Construction time = {:.3f} min.'.format(float(construction_time)))
 
-        power_consumption = Fraction(input_def('What is the power consumption?','3400'))
+        power_consumption = Fraction(input_def('What is the power consumption?', '3400'))
         print('Number of power plants: {:10.3f}'.format(float(power_consumption/1120)))
         temp = dict_bp()
         necessary_items_for_construction_nuclear = dict_bp()
         for k, v in nuclear_power_plant_1120MW.items():
             temp += dict_bp({k: float(power_consumption / 1120 * v)})
-            necessary_items_for_construction_nuclear += dict_bp({k: power_consumption / 1120 * v / construction_time})
-        debug(json.dumps(necessary_items_for_construction + temp, indent=4, sort_keys=True))
+            new_val = power_consumption / 1120 * v / construction_time
+            necessary_items_for_construction_nuclear += dict_bp({k: new_val})
+        debug(json.dumps(necessary_items_for_construction + temp,
+                         indent=4, sort_keys=True))
 
         recipes = get_recipes()
         exclusion = ('speed-module-3', 'productivity-module-3')
         for item_name, amount in necessary_items_for_construction.items():
-            if not item_name in exclusion:
+            if item_name not in exclusion:
                 necessary_items_for_construction1[item_name] = necessary_items_for_construction[item_name] / construction_time
         necessary_items_for_construction1 += necessary_items_for_construction_nuclear
 
@@ -249,11 +261,8 @@ if __name__ == "__main__":
             par = '{}:r:{},'.format(item_name, performance)
             settings += par
         settings += additional_parameters
-        
-        #print(settings)
+
+        # print(settings)
         zip = js_bytes_to_string(js_btoa(pako_deflate_raw(settings)))
-        link = 'kirkmcdonald.github.io/calc.html#zip={}'.format(zip) 
+        link = 'kirkmcdonald.github.io/calc.html#zip={}'.format(zip)
         print(link)
-
-
-        
