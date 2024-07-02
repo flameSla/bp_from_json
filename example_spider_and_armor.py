6d25fbaaefaaece8b4982c234e3cc285eb473aee
flameSla <39356103+flameSla@users.noreply.github.com>
2024-07-02 18:45:27 +0700

    Update example_spider_and_armor.py 18:45 02/07/2024

--- example_spider_and_armor.py
+++ example_spider_and_armor.py
@@ -4,16 +4,9 @@ from bp_from_json import get_recipes_with_one_product
 from bp_from_json import get_items
 import math
 from fractions import Fraction
-from bp_functions import get_iningredients
+
 from bp_functions import add_machine
-from bp_functions import add_passive_provider
-from bp_functions import add_filter_inserter
 from bp_functions import add_inserter
-from bp_functions import new_connection
-from bp_functions import new_circuit_condition
-from bp_functions import get_stack_size
-from bp_functions import add_logistic_chest_requester
-from bp_functions import update_request_filters
 from bp_functions import add_entity
 
 
